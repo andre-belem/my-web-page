aa0049b693652b849ef852b7a753906475ffa4cd
andre-belem <br.andrebelem@gmail.com>
2026-06-01 22:13:42 -0300

    Add Dashboard Examples

---
+++ code/es-modules/Data/DataTableOptions.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Options to initialize a new DataTable instance.
+ */
+export interface DataTableOptions {
+    /**
+     * Initial map of column aliases to original column names.
+     */
+    aliases?: Record<string, string>;
+    /**
+     * Initial columns with their values.
+     */
+    columns?: Record<string, Array<DataTableValue>>;
+    /**
+     * Custom ID to identify the new DataTable instance.
+     */
+    id?: string;
+    /**
+     * Name of the (invisible) column that contains all row keys.
+     */
+    rowKeysId?: string;
+}
+export type DataTableValue = (boolean | null | number | string | undefined);
+export default DataTableOptions;
---
+++ code/es-modules/masters/modules/math-modifier.src.js
@@ -0,0 +1,32 @@
+/**
+ * @license Highcharts Dashboards Math 2.2.0 (2024-07-02)
+ * @module dashboards/modules/math-modifier
+ * @requires dashboards
+ *
+ * (c) 2009-2024 Highsoft AS
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+/* *
+ *
+ *  Imports
+ *
+ * */
+import Globals from '../../Dashboards/Globals.js';
+import Formula from '../../Data/Formula/Formula.js';
+// Fill registries
+import '../../Data/Modifiers/MathModifier.js';
+/* *
+ *
+ *  Namespace
+ *
+ * */
+const G = Globals;
+G.Formula = Formula;
+/* *
+ *
+ *  Default Export
+ *
+ * */
+export default G;
